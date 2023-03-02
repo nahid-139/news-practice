f08eb2006b70a67ed0bd9fbc6dfdd49fb3d0af48
nahid139 <nahidnhptake@gmail.com>
2023-03-02 14:21:36 -0700

    categories added

---
+++ js/news.js
@@ -0,0 +1,40 @@
+const fetchCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+        .then((res) => res.json())
+        .then((data) => showCategories(data.data))
+
+
+};
+
+const showCategories = data => {
+    // console.log(data)
+
+    const categoriesContainer = document.getElementById('categories-container');
+    data.news_category.forEach(singleCategory => {
+        // console.log(singleCategory)
+
+        let linkContainer = document.createElement('p')
+        linkContainer.innerHTML = ` <a class="nav-link" href="#" onclick ="fetchCategoriesNews('${singleCategory.category_id}','${singleCategory.category_name}')">${singleCategory.category_name}</a>`;
+        categoriesContainer.appendChild(linkContainer);
+    });
+}
+// fetch all newses
+
+const fetchCategoriesNews = (category_id,category_name) => {
+    // console.log(category_id)
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAllNews(data.data,category_name));
+}
+const showAllNews =(data,category_name) =>{
+    console.log(data,category_name)
+
+    document.getElementById('news-count').innerText = data.length;
+    document.getElementById('category-name').innerText = category_name;
+
+    data.forEach(singleNews =>{
+
+        console.log(singleNews)
+    })
+}
